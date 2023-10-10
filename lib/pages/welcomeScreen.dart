23713879d9eb7053323f633218624e8c514f0f86
Abderrahmane <abderrahmanekihal5@gmail.com>
2023-10-10 14:17:18 +0100

    initial commit

---
+++ lib/pages/welcomeScreen.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
